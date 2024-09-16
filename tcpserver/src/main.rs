bfeb6ea5112916435950c2dff863e000f477ac55
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-16 14:53:44 +0200

    feat: Write the TCP Server

--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -1,3 +1,10 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let connection_listener = TcpListener::bind("127.0.0.1:3001").unwrap();
+    println!("Listening on 127.0.0.1:3001");
+    for stream in connection_listener.incoming() {
+        let _stream = stream.unwrap();
+        println!("Connection established");
+    }
 }
