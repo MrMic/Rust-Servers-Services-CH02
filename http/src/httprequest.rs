094e066efb607b98e7471d3c9d796f7ac0b87c61
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-19 14:46:58 +0200

    feat: HTTP Request Parsing - PART 2

--- http/src/httprequest.rs
+++ http/src/httprequest.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 // ______________________________________________________________________
 #[derive(Debug, PartialEq)]
 pub enum Method {
@@ -34,7 +36,80 @@ impl From<&str> for Version {
     }
 }
 
-// * TEST: ______________________________________________________________________
+// ______________________________________________________________________
+#[derive(Debug, PartialEq)]
+pub enum Resource {
+    Path(String),
+}
+
+// ______________________________________________________________________
+#[derive(Debug)]
+pub struct HttpRequest {
+    pub method: Method,
+    pub version: Version,
+    pub resource: Resource,
+    pub headers: HashMap<String, String>,
+    pub msg_body: String,
+}
+
+impl From<String> for HttpRequest {
+    fn from(req: String) -> Self {
+        let mut parsed_method = Method::Uninitialized;
+        let mut parsed_version = Version::V1_1;
+        let mut parsed_resource = Resource::Path("".to_string());
+        let mut parsed_headers = HashMap::new();
+        let mut parsed_msg_body = "";
+
+        // Read each line in the incoming HTTP HttpRequest
+        for line in req.lines() {
+            // If the line read is request line, call function process_request_line()
+            if line.contains("HTTP") {
+                let (method, resource, version) = process_request_line(line);
+                parsed_method = method;
+                parsed_version = version;
+                parsed_resource = resource;
+                // if line read is a header line, call function process_header_line()
+            } else if line.contains(":") {
+                let (key, value) = process_header_line(line);
+                parsed_headers.insert(key, value);
+                // if it is a blank line, do nothing
+            } else if line.len() == 0 {
+                // If none of these, treat it as message body
+            } else {
+                parsed_msg_body = line;
+            }
+        }
+        // Parse the incoming HTTP Request into HttpRequest struct
+        HttpRequest {
+            method: parsed_method,
+            version: parsed_version,
+            resource: parsed_resource,
+            headers: parsed_headers,
+            msg_body: parsed_msg_body.to_string(),
+        }
+    }
+}
+
+fn process_request_line(s: &str) -> (Method, Resource, Version) {
+    // Parse the request line into individual chunks split by whitespaces.
+    let mut words = s.split_whitespace();
+    // Extract the HTTP method from first part of the request line
+    let method = words.next().unwrap();
+    // Extract the resource (URI/URL) from second part of the request line
+    let resource = words.next().unwrap();
+    // Extract the HTTP version from third part of the request line
+    let version = words.next().unwrap();
+
+    (
+        method.into(),
+        Resource::Path(resource.to_string()),
+        version.into(),
+    )
+}
+
+fn process_header_line() {}
+
+// * TEST: ═══════════════════════════════════════════════════════════
 #[cfg(test)]
 mod tests {
     use super::*;
