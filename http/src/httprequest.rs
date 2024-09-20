ee719503bee27da95b1553395a1367280828979f
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-20 14:50:31 +0200

    feat: HTTP Request Parsing + Tests - PART 3

--- http/src/httprequest.rs
+++ http/src/httprequest.rs
@@ -107,7 +107,21 @@ fn process_request_line(s: &str) -> (Method, Resource, Version) {
     )
 }
 
-fn process_header_line() {}
+fn process_header_line(s: &str) -> (String, String) {
+    // Parse the header line into words split by separator (':')
+    let mut header_items = s.split(":");
+    let mut key = String::from("");
+    let mut value = String::from("");
+    // Extract the key part of the header line
+    if let Some(k) = header_items.next() {
+        key = k.to_string();
+        // Extract the value part of the header line
+        if let Some(v) = header_items.next() {
+            value = v.to_string();
+        }
+    }
+    (key, value)
+}
 
 // * TEST: ═══════════════════════════════════════════════════════════
 #[cfg(test)]
@@ -124,5 +138,23 @@ mod tests {
     fn test_version_into() {
         let v: Version = "HTTP/1.1".into();
         assert_eq!(v, Version::V1_1);
+    }
+
+    #[test]
+    fn test_read_http() {
+        let s: String = String::from(
+            "GET /greeting HTTP/1.1\r\nHost:localhost:3000\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n",
+        );
+
+        let mut headers_expected: HashMap<String, String> = HashMap::new();
+        headers_expected.insert("User-Agent".into(), " curl/7.64.1".into());
+        headers_expected.insert("Host".into(), "localhost".into());
+        headers_expected.insert("Accept".into(), " */*".into());
+
+        let request: HttpRequest = s.into();
+        assert_eq!(Method::Get, request.method);
+        assert_eq!(Version::V1_1, request.version);
+        assert_eq!(Resource::Path("/greeting".to_string()), request.resource);
+        assert_eq!(headers_expected, request.headers);
     }
 }
