9e5819be3f5b333a26c8ba3edd5021d076f00a6e
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-30 14:55:21 +0200

    feat: Write the router & handler module

--- httpserver/src/router.rs
+++ httpserver/src/router.rs
@@ -4,16 +4,32 @@ use http::{
 };
 use std::io::prelude::*;
 
-use super::handler::{Handler, PageNotFoundHandler, StaticPageHandler, WebServiceHandler};
+use crate::handler::{Handler, PageNotFoundHandler, StaticPageHandler, WebServiceHandler};
 
 pub struct Router;
 
 impl Router {
-    pub fn route(req: HttpRequest, stream: &mut impl Write) -> () {
-        // TODO: Implement Router
-        match req.method() {
+    pub fn route(req: HttpRequest, stream: &mut impl Write) {
+        match req.method {
             // If GET request
-            httprequest::Method::Get => match &req.resource {},
+            httprequest::Method::Get => match &req.resource {
+                httprequest::Resource::Path(s) => {
+                    // Parse the URI
+                    let route: Vec<&str> = s.split("/").collect();
+                    match route[1] {
+                        // If the route begin with /api, invoke Web WebService
+                        "api" => {
+                            let resp: HttpResponse = WebServiceHandler::handle(&req);
+                            let _ = resp.send_response(stream);
+                        }
+                        // Else invoke Static Page Handler
+                        _ => {
+                            let resp: HttpResponse = StaticPageHandler::handle(&req);
+                            let _ = resp.send_response(stream);
+                        }
+                    }
+                }
+            },
             // If method is not GET request, return 404 page
             _ => {
                 let resp: HttpResponse = PageNotFoundHandler::handle(&req);
