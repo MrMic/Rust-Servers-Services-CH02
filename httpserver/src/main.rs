1a1e15b977519700b265ef8e68893a260e52d678
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-30 15:06:56 +0200

    feat(UI/UX): Create HTML & JSON pages for tests

--- httpserver/src/main.rs
+++ httpserver/src/main.rs
@@ -6,7 +6,7 @@ mod server;
 
 fn main() {
     // Start  a server
-    let server = Server::new("127.0.0.1:3000");
+    let server = Server::new("127.0.0.1:3005");
     // Run the server w
     server.run();
 }
